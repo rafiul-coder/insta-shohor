55dda78830aad8ee66698df187a73b3f3a66fbf1
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-09-16 19:26:01 +0600

    test

--- js/app.js
+++ js/app.js
@@ -155,7 +155,8 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
-  posts.forEach((post) => {
+  document.getElementById("reported").textContent = '';
+  reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("reported").appendChild(div);
   });
